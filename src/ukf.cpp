049b72f66398df76316eee4b5c38e11bb5a44969
Shereen Elkordi <selkordi@mcit.gov.eg>
2022-01-08 02:16:52 +0200

    modifying process noise

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -22,10 +22,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 2.0;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 2.0;
   
   /**
    * DO NOT MODIFY measurement noise values below.
